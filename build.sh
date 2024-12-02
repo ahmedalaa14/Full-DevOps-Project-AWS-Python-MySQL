173bd5ee269c6e7b660c8f18220a5ed040bbf76c
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-12-02 19:23:05 +0200

    script to build everything

---
+++ build.sh
@@ -0,0 +1,61 @@
+#!/bim/bash
+
+d terraform && \
+terraform init 
+terraform apply -auto-approve
+cd ..
+
+# update kubeconfig
+echo "--------------------Update Kubeconfig--------------------"
+aws eks update-kubeconfig --name $cluster_name --region $region
+
+# remove preious docker images
+echo "--------------------Remove Previous build--------------------"
+docker rmi -f $app_image_name || true
+docker rmi -f $db_image_name || true
+
+# build new docker image with new tag
+echo "--------------------Build new Image--------------------"
+docker build -t $app_image_name todo-app/
+docker build -f k8s/Dockerfile.mysql -t $db_image_name k8s
+
+# ECR Login
+echo "--------------------Login to ECR--------------------"
+aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $aws_id.dkr.ecr.eu-central-1.amazonaws.com
+
+# push the latest build to dockerhub
+echo "--------------------Pushing Docker Image--------------------"
+docker push $app_image_name
+docker push $db_image_name
+
+# create namespace
+echo "--------------------creating Namespace--------------------"
+kubectl create ns $namespace || true
+
+# add rds endpoint into k8s secrets
+echo "--------------------Create RDS Secrets --------------------"
+kubectl create secret -n $namespace generic rds-endpoint --from-literal=endpoint=$rds_endpoint || true
+kubectl create secret -n $namespace generic rds-username --from-literal=username=$db_username || true
+kubectl create secret -n $namespace generic rds-password --from-literal=password=$db_password || true
+
+# deploy the application
+echo "--------------------Deploy App--------------------"
+kubectl apply -n $namespace -f k8s/
+
+# Wait for application to be deployed
+echo "--------------------Wait for all pods to be running--------------------"
+sleep 60s
+
+# Get ingress URL
+echo "--------------------Application LoadBalancer URL--------------------"
+kubectl get svc -n ${namespace} ${app_service_name} -o=custom-columns=EXTERNAL-IP:.status.loadBalancer.ingress[*].hostname | tail -n +2
+echo "-------------------- Alertmanager LoadBalancer URL--------------------"
+kubectl get svc -n ${monitoring_ns} ${alertmanager_service_name} -o=custom-columns=EXTERNAL-IP:.status.loadBalancer.ingress[*].hostname | tail -n +2
+echo "--------------------Prometheus LoadBalancer URL--------------------"
+kubectl get svc -n ${monitoring_ns} ${prometheus_service_name} -o=custom-columns=EXTERNAL-IP:.status.loadBalancer.ingress[*].hostname | tail -n +2
+echo "--------------------Grafana LoadBalancer URL--------------------"
+kubectl get svc -n ${monitoring_ns} ${grafana_service_name} -o=custom-columns=EXTERNAL-IP:.status.loadBalancer.ingress[*].hostname | tail -n +2
+
+# Get RDS endpoint URL
+echo "--------------------RDS endpoint URL--------------------"
+echo $rds_endpoint
